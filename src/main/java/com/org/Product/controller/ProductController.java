4e09e2a59aae845b7c049ebdf4a3d626c53deca2
KruthikaPPatil <patilkruthikap@gmail.com>
2024-12-17 18:08:14 +0530

    delete and update done

--- src/main/java/com/org/Product/controller/ProductController.java
+++ src/main/java/com/org/Product/controller/ProductController.java
@@ -2,11 +2,15 @@ package com.org.Product.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -58,6 +62,21 @@ public class ProductController {
 		@GetMapping("/products/stock/{min}/{max}")
 		public ResponseEntity<Object> fetchByStockBetween(@PathVariable int min,@PathVariable int max){
 			return service.fetchByStockBetween(min,max);
+		}
+		
+		@DeleteMapping("/products/{id}")
+		public ResponseEntity<Object> deleteById(@PathVariable int id){
+			return service.deleteById(id);
+		}
+		
+		@PutMapping("/products")
+		public ResponseEntity<Object> updateRecord(@RequestBody Product product){
+			return service.updateProduct(product);
+		}
+		
+		@PatchMapping("/products/{id}")
+		public ResponseEntity<Object> updateRecord(@PathVariable int id,@RequestBody Product product){
+			return service.updateProduct(id,product);
 		}
 		
 }
